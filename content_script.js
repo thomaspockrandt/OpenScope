dbbeccd68496df378b26bbc2904fa21d6ba1730c
Thomas Pockrandt <me@thomaspockrandt.com>
2017-04-29 22:34:08 +0200

    Load logo

--- content_script.js
+++ content_script.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
   console.log(1);
-  var bar = '<div id="zeisshack-bar"><div id="openscope-logo"><img src="openscope-logo.png" /></div></div>';
+  var logo_url = chrome.extension.getURL("openscope-logo.png");
+  var bar = '<div id="zeisshack-bar"><div id="openscope-logo"><img src="' + logo_url + '" /></div></div>';
   $("body").append(bar);
 });
