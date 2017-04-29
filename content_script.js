aac44506edf61b985384a67dee2c50c316c56eb1
Donnie Ray Crisp <donnieraycrisp@me.com>
2017-04-29 22:24:22 +0200

    add basic skeleton

--- content_script.js
+++ content_script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
   console.log(1);
-  var bar = '<div id="zeisshack-bar">OpenScope</div>';
+  var bar = '<div id="zeisshack-bar"><div id="openscope-logo"><img src="openscope-logo.png" /></div></div>';
   $("body").append(bar);
 });
