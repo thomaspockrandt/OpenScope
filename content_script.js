98997e778201b4b832e6e8eeaeb9852bfb55957b
Donnie Ray Crisp <donnieraycrisp@me.com>
2017-04-30 11:35:37 +0200

    focus textarea

--- content_script.js
+++ content_script.js
@@ -36,6 +36,7 @@ $(document).ready(function() {
           $("#popup").css({"left": x, "top": y});
           $("#pin").css({"left": x, "top": y});
           $("#popup").addClass("active");
+          $("#popup textarea").focus();
         }
 
         $(document.body).append(div);
