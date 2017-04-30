0390b47a57640bc22f36901fb4ff633ef9942a38
Donnie Ray Crisp <donnieraycrisp@me.com>
2017-04-30 10:04:56 +0200

    show avatar names on hover and add image blocker

--- content_script.js
+++ content_script.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
   var avatar2 = chrome.extension.getURL("avatar-2.png");
   var avatar3 = chrome.extension.getURL("avatar-3.png");
   var avatar4 = chrome.extension.getURL("avatar-4.png");
-  var bar = '<div id="zeisshack-bar"><div id="openscope-logo"><img src="' + logo_url + '" /></div><div id="collaborators"><div class="avatar active"><img src="' + avatar1 + '" /></div><div class="avatar active"><img src="' + avatar2 + '" /></div><div class="avatar"><img src="' + avatar3 + '" /></div><div class="avatar"><img src="' + avatar4 + '" /></div></div><div id="meta"><div id="comment-button">Comment</div></div></div>';
+  var bar = '<div id="blocker"></div><div id="zeisshack-bar"><div id="openscope-logo"><img src="' + logo_url + '" /></div><div id="collaborators"><div class="avatar active"><div class="name"><div class="inside">Jessica</div></div><img src="' + avatar1 + '" /></div><div class="avatar active"><div class="name"><div class="inside">Jeremy</div></div><img src="' + avatar2 + '" /></div><div class="avatar"><div class="name"><div class="inside">Randolph</div></div><img src="' + avatar3 + '" /></div><div class="avatar"><div class="name"><div class="inside">Rebecca</div></div><img src="' + avatar4 + '" /></div></div><div id="meta"><div id="comment-button">Comment</div></div></div>';
   $("body").append(bar);
 
   $(".avatar").on("click", function() {
@@ -13,6 +13,14 @@ $(document).ready(function() {
   });
 
   $("#comment-button").on("click", function() {
+    var blocker = document.getElementById("blocker");
+
+    if (blocker.classList.contains("active")) {
+      blocker.classList.remove("active");
+    } else {
+      blocker.classList.add("active");
+    }
+
     console.log(4);
   })
 });
